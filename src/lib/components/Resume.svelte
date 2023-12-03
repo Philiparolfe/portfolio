c79bb4f7c19595d94b0a8800e8915804aadfc40a
Philip Rolfe <philip.rolfe@icloud.com>
2023-12-02 19:01:00 -0500

    modified resume and theme

--- src/lib/components/Resume.svelte
+++ src/lib/components/Resume.svelte
@@ -1,113 +1,108 @@
-<section id="projects" class="flex justify-center items-center my-7 lg:mx-32">
+<section id="projects" class="flex my-7 lg:mx-32">
 	<div class="p-4 card">
-		<div class="">
+		<div class="flex flex-col gap-3">
 			<h1 id="resume" class="h1 text-primary-500 mb-4 text-center">Resume</h1>
-			<div class="container mx-auto p-4">
 
-				<!-- Header -->
-				<div class="text-4xl font-bold mb-4">
-				  Philip Rolfe
+			<div class="flex flex-row justify-between place-content-between">
+				<div class="p-1">
+					<h2 class="h2">Objective</h2>
+					<p class="">
+						Dedicated and detail-oriented student seeking part-time employment in a dynamic role.
+						Eager to apply my skills and knowledge in identifying, analyzing, and maintaining secure
+						computing environments. Open to diverse opportunities that allow me to contribute
+						effectively while gaining valuable experience in a part-time capacity.
+					</p>
 				</div>
-			
-				<!-- Contact Information -->
-				<div class="mb-4">
-				  <div class="font-bold">Contact</div>
-				  <div>Brampton, ON</div>
-				  <div>(phone number hidden)</div>
-				  <div>philip.rolfe@georgebrown.ca</div>
+			</div>
+			<div class="flex flex-row justify-between place-content-between">
+				<div class="p-1">
+					<h2 class="h2">Education</h2>
+					<p>George Brown College</p>
+					<p>Toronto, Ontario</p>
+					<p>Advanced Diploma in Computer Systems Technology 2025 (current student)</p>
+					<br />
+					<p>Peel Alternative North</p>
+					<p>Brampton, Ontario</p>
+					<p>OSSGD 2017</p>
 				</div>
-			
-				<!-- Objective -->
-				
-			
-				<!-- Education -->
-				<div class="mb-4">
-				  <div class="font-bold">Education</div>
-				  			
-				  <div class="mt-2">Advanced diploma in Computer Systems Technology, George Brown College</div>
-				  <div>Expected Graduation: 2025 (currently enrolled)</div>
-			
-				  <div class="mt-2">Google IT Support Professional Certificate</div>
-				  <div>April 13, 2021</div>
-				</div>
-			
-				<!-- Technical Skills -->
-				<div class="mb-4">
-				  <div class="font-bold">Technical Skills</div>
-				  <p>
-					Computer Hardware: Diagnosing, Repairing, Maintenance<br>
-					Security Technologies: Firewall, Encryption, SSH, SSL<br>
-					Operating Systems: Windows Server 2016, Enterprise Linux (openSUSE)<br>
-					Networking: Routing and switching, network fundamentals, LAN Switching technologies, routing technologies, WAN technologies, network security, troubleshooting, VLANs, VPNs, STP<br>
-					Programming: Python, JavaScript, SQL<br>
-					Applications: Microsoft Office Suite
-				  </p>
-				</div>
-			
-				<!-- Soft Skills -->
-				<div class="mb-4">
-				  <div class="font-bold">Soft Skills</div>
-				  <p>
-					Strong Analytical and Critical Thinking.<br>
-					Effective Communication and Team Collaboration.<br>
-					Adaptability and Continuous Learning.<br>
-					Problem-Solving.
-				  </p>
-				</div>
-			
-				<!-- Hands-On Experience -->
-				<div class="mb-4">
-				  <div class="font-bold">Hands-On Experience</div>
-				  <p>
-					Networking Labs:<br>
-					Computer Systems Technology Program, George Brown College<br>
-					- Engaged in hands-on Cisco labs focused on network optimization.<br>
-					- Implemented and configured Cisco networking solutions to enhance network performance and security.<br>
-			
-					Academic Projects:<br>
-					Computer Systems Technology Program, George Brown College<br>
-					- Developed and presented IT best practices.<br>
-					- Explored key networking concepts and their applications within the Windows Server environment.<br>
-					- Automated network and system configurations using Python.<br>
-					- Monitored network and systems using Python.<br>
-					- Developed and delivered an in-depth presentation on networking services available in Windows Server 2016.<br>
-					- Configured and optimized various server environments ensuring efficient and secure performance.
-				  </p>
-				</div>
-			
-				<!-- Work Experience -->
-				<div class="mb-4">
-				  <div class="font-bold">Work Experience</div>
-			
-				  <!-- Tini’s Treats -->
-				  <div class="mt-2">
-					<div>Tini’s Treats | Farmers Market | Milton | 2023</div>
-					<ul class="list-disc list-inside">
-					  <li>Provided customer service by answering questions, offering recommendations, and handling transactions.</li>
-					  <li>Ensured the cleanliness and organization of the market area.</li>
-					  <li>Assisted with the packing and unpacking of goods.</li>
-					  <li>Demonstrated strong communication skills and maintained a positive attitude while interacting with vendors and customers.</li>
+				<div class="p-1">
+					<h2 class="h2">Academic Projects</h2>
+					<p>Computer Systems Technology Program, George Brown College</p>
+					<ul class=" list">
+						<li>Developed and presented IT best practices.</li>
+						<li>
+							Explored key networking concepts and their applications within both a Windows Server
+							environment and a Linux environment.
+						</li>
+						<li>Automated network and system configurations using Python.</li>
+						<li>Monitored network and systems using Python.</li>
+						<li>
+							Developed and delivered an in-depth presentation on networking services available in
+							Windows Server 2016.
+						</li>
+						<li>
+							Configured and optimized various server environments ensuring efficient and secure
+							performance.
+						</li>
 					</ul>
-				  </div>
-			
-				  <!-- Order Picker -->
-				  <div class="mt-2">
-					<div>Order Picker | Coca-Cola | Brampton | 2021</div>
-					<ul class="list-disc list-inside">
-					  <li>Utilized RF scanners to locate and retrieve specific products from inventory.</li>
-					  <li>Ensured accurate picking of customer orders based on established guidelines.</li>
-					  <li>Conducted regular inventory checks and maintained accurate records.</li>
-					  <li>Followed safety protocols and maintained a clean and organized work area.</li>
+				</div>
+			</div>
+			<div class="flex justify-center">
+				<div class="p-1">
+					<h2 class="h2">Technical Skills</h2>
+					<ul class="list">
+						<li>Security Technologies: Firewall, Encryption, antivirus</li>
+						<li>Operating Systems: Windows Server 2016, Enterprise Linux (openSUSE)</li>
+						<li>
+							Networking: network infrastructure, routing/switching technologies, WAN technologies,
+							network security, troubleshooting, VLANs, VPNs
+						</li>
+						<li>Databases: Postgres, SQLite</li>
+						<li>Scripting: PowerShell, Bash, Python, JavaScript</li>
+						<li>Web Technologies: HTML, CSS, Svelte</li>
+						<li>Applications: Microsoft Office Suite, Microsoft Sentinel</li>
 					</ul>
-				  </div>
 				</div>
-			
-				<!-- References -->
-				<div>
-				  <div class="font-bold">References available upon request</div>
+
+				<div class="p-1">
+					<h2 class="h2">Soft Skills</h2>
+					<ul>
+						<li>Strong Analytical and Critical Thinking</li>
+						<li>Effective Communication and Team Collaboration</li>
+						<li>Adaptability and Continuous Learning</li>
+						<li>Problem-Solving</li>
+					</ul>
 				</div>
-			
-			  </div>
+			</div>
+			<h2 class=" h2">Work Experience</h2>
+
+			<h3>Tini’s Treats | Farmers Market | Milton | 2023</h3>
+			<ul class=" list ml-5">
+				<li>
+					Provided customer service by answering questions, offering recommendations, and handling
+					transactions.
+				</li>
+				<li>Ensured the cleanliness and organization of the market area.</li>
+				<li>Assisted with the packing and unpacking of goods.</li>
+				<li>
+					Demonstrated strong communication skills and maintained a positive attitude while
+					interacting with vendors and customers.
+				</li>
+			</ul>
+			<br />
+			<h3>Tini’s Treats | Farmers Market | Woodbridge | 2022</h3>
+			<ul class=" list ml-5">
+				<li>
+					Offered outstanding customer service by addressing inquiries, suggesting coffee choices,
+					and managing transactions.
+				</li>
+				<li>Ensured a tidy and well-organized coffee stand.</li>
+				<li>Assisted in packing and unpacking goods.</li>
+				<li>
+					Showcased robust communication skills and maintained a positive attitude in interactions
+					with both vendors and customers.
+				</li>
+			</ul>
 		</div>
 	</div>
 </section>
