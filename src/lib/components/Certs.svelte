7d11c56a1921e6e497adc29db4c7dc7c4029fa2b
Philip Rolfe <philip.rolfe@icloud.com>
2023-12-04 03:02:56 -0500

    added more links

--- src/lib/components/Certs.svelte
+++ src/lib/components/Certs.svelte
@@ -7,7 +7,9 @@
 		<div class="p-3 ">
 			<h1 id="certs" class="h1 text-primary-500 mb-4 text-center">Certifications</h1>
             <div class="wrapper">
+				<a href="https://www.coursera.org/verify/RQ2CPHZ8JER6" target="_blank" rel="noopener noreferrer">
                 <img src={cert} alt="Google Technical Support Fundementals">
+			</a>
             </div>
 		</div>
 	</div>
